0faff65961a7cd860eaba1b06c98728943fc3b78
Scott Hodgson <Scott-D-Hodgson@users.noreply.github.com>
2019-04-07 14:09:00 -0400

    Added base/default theme SASS (#3)

--- gruntfile.js
+++ gruntfile.js
@@ -2,11 +2,20 @@
 module.exports = function(grunt) {
 
     grunt.config('theme', (function(){
-        var choice = grunt.option('theme') || 'bootstrap';
+        var choice = grunt.option('theme') || 'default';
         var theme = { 
-            name: "bootstrap", 
-            source_sass: "node_modules/bootstrap-sass/assets/stylesheets", 
-            target_sass: "bootstrap/stylesheets"};
+            name: "wet-boew", 
+            source_sass: "node_modules/wet-boew-theme-default/stylesheets", 
+            target_sass: "wet-boew-default/stylesheets"};
+        switch (choice) {
+            case 'goc-intranet': 
+                theme = {
+                    name: "goc-intranet", 
+                    source_sass: "node_modules/wet-boew-theme-goc-intranet/stylesheets", 
+                    target_sass: "goc-intranet/stylesheets"
+                };
+                break;
+        }
         return theme;
     })())
 
@@ -32,16 +41,30 @@ module.exports = function(grunt) {
         // command: grunt copy
         // result: 
         copy: {
-            stylesheets: {
+            bootstrap: {
+                cwd: 'node_modules/bootstrap-sass/assets/stylesheets',
+                src: '**',
+                dest: 'wip/theme-' + grunt.config('theme').name + '/bootstrap/stylesheets',
+                expand: true,
+                flatten: false
+            },
+            base: {
+                cwd: 'node_modules/wet-boew-theme-base/stylesheets',
+                src: '**',
+                dest: 'wip/theme-' + grunt.config('theme').name + '/base/stylesheets',
+                expand: true,
+                flatten: false
+            },
+            theme: {
                 cwd: grunt.config('theme').source_sass,
                 src: '**',
-                dest: "wip/theme-" + grunt.config('theme').name + "/" + grunt.config('theme').target_sass,
+                dest: 'wip/theme-' + grunt.config('theme').name + '/' + grunt.config('theme').target_sass,
                 expand: true,
                 flatten: false
             } 
         }
     });
 
-    grunt.registerTask('build', ['clean', 'copy:stylesheets']);
+    grunt.registerTask('build', ['clean', 'copy']);
 
 }

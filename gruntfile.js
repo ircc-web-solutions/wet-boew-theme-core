1a16ca38f38d96c82e9928960baa4dea72c4b533
Scott Hodgson <5344448@gmail.com>
2019-04-19 07:46:24 -0400

    Added download of zip themes from Github

--- gruntfile.js
+++ gruntfile.js
@@ -3,24 +3,30 @@ module.exports = function(grunt) {
 
     grunt.config('theme', (function(){
         var choice = grunt.option('theme') || 'default';
-        var theme = { 
-            name: "wet-boew", 
-            source_sass: "node_modules/wet-boew-theme-default/stylesheets", 
-            target_sass: "wet-boew-default/stylesheets"};
+        var theme;
         switch (choice) {
+            case 'default':
+            case 'wet-boew':
+                theme = { 
+                    name: 'wet-boew', 
+                    repository: 'ircc-web-solutions/theme-wet-boew'};
             case 'goc-intranet': 
                 theme = {
                     name: "goc-intranet", 
-                    source_sass: "node_modules/wet-boew-theme-goc-intranet/stylesheets", 
-                    target_sass: "goc-intranet/stylesheets"
-                };
+                    repository: 'ircc-web-solutions/theme-goc-intranet'};
                 break;
+            default:
+                theme = { 
+                    name: 'unregistered-repo', 
+                    repository: choice};
         }
         return theme;
     })())
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-wget');
+    grunt.loadNpmTasks('grunt-zip');
 
     // project configuration
     grunt.initConfig({
@@ -36,35 +42,20 @@ module.exports = function(grunt) {
             dist: ["dist"],
             wip: ["wip"]
         },
-        // configuration for grunt-contrib-copy
-        //
-        // command: grunt copy
-        // result: 
-        copy: {
-            bootstrap: {
-                cwd: 'node_modules/bootstrap-sass/assets/stylesheets',
-                src: '**',
-                dest: 'wip/theme-' + grunt.config('theme').name + '/bootstrap/stylesheets',
-                expand: true,
-                flatten: false
-            },
-            base: {
-                cwd: 'node_modules/wet-boew-theme-base/stylesheets',
-                src: '**',
-                dest: 'wip/theme-' + grunt.config('theme').name + '/base/stylesheets',
-                expand: true,
-                flatten: false
-            },
+        wget: {
             theme: {
-                cwd: grunt.config('theme').source_sass,
-                src: '**',
-                dest: 'wip/theme-' + grunt.config('theme').name + '/' + grunt.config('theme').target_sass,
-                expand: true,
-                flatten: false
-            } 
+                files: {
+                    'wip/base.zip': 'https://github.com/ircc-web-solutions/theme-base/archive/master.zip',
+                    'wip/theme.zip': 'https://github.com/' + grunt.config('theme').repository + '/archive/master.zip',
+                    'wip/src/wet-boew.zip': 'https://github.com/wet-boew/wet-boew/archive/master.zip'
+                }
+            }
+        },
+        unzip: {
+            'wip/theme/': 'wip/theme.zip'
         }
     });
 
-    grunt.registerTask('build', ['clean', 'copy']);
+    grunt.registerTask('build', ['clean', 'wget', 'unzip']);
 
 }

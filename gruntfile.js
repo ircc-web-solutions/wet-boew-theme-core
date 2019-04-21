524dfcd963e1f657ed0668224efa1865cda51d0e
Scott Hodgson <Scott-D-Hodgson@users.noreply.github.com>
2019-04-21 11:49:10 -0400

    Added initial CSS processing (#7)

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,7 @@
-//
 module.exports = function(grunt) {
+
+    const sass = require('node-sass');
+    require('time-grunt')(grunt);
 
     grunt.config('theme', (function(){
         var choice = grunt.option('theme') || 'default';
@@ -27,35 +29,199 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-wget');
     grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.loadNpmTasks('grunt-string-replace');
 
     // project configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        // configuration for grunt-contrib-clean
-        //
-        // command: grunt clean
-        // result: all folders will be emptied/removed
-        //
-        // command: grunt clean:folder_dist
-        // result: folder 'dist' will be emptied/removed
+		coreDist: 'dist/wet-boew',        
+        themeDist: 'dist/theme-wet-boew',
+        copy: {
+            prepare: {
+                files:
+                [ 
+                    {
+                        cwd: 'node_modules/bootstrap-sass/assets/stylesheets',
+                        src: ['**/*.scss'],
+                        dest: 'src/bootstrap/stylesheets',
+                        expand: true,
+                        flatten: false
+                    },
+                    {
+                        cwd: 'wip/repo/wet-boew/wet-boew-master/src',
+                        src: ['**/*.scss', '!**/demo/**', '!plugins/**'],
+                        dest: 'src/wet-boew/stylesheets',
+                        expand: true,
+                        flatten: false
+                    },
+                    {
+                        cwd: 'wip/repo/wet-boew/wet-boew-master/src/plugins',
+                        src: ['**/*.scss', '!**/demo/**'],
+                        dest: 'src/plugins/stylesheets',
+                        expand: true,
+                        flatten: false
+                    },
+                    {
+                        cwd: 'wip/repo/wet-boew/wet-boew-master/theme',
+                        src: ['**/*.scss', '!**/demo/*.scss'],
+                        dest: 'src/theme-wet-boew/stylesheets',
+                        expand: true,
+                        flatten: false
+                    }
+                ]
+            }
+        },
         clean: {
-            dist: ["dist"],
-            wip: ["wip"]
+            prepare: ['wip', 'src', 'dist'],
+            finalize: ['wip', 'src']
         },
         wget: {
-            theme: {
+            prepare: {
                 files: {
                     'wip/base.zip': 'https://github.com/ircc-web-solutions/theme-base/archive/master.zip',
                     'wip/theme.zip': 'https://github.com/' + grunt.config('theme').repository + '/archive/master.zip',
-                    'wip/src/wet-boew.zip': 'https://github.com/wet-boew/wet-boew/archive/master.zip'
+                    'wip/zip/wet-boew.zip': 'https://github.com/wet-boew/wet-boew/archive/master.zip'
                 }
             }
         },
         unzip: {
-            'wip/theme/': 'wip/theme.zip'
+            prepare: {
+                files: {
+                    'wip/repo/wet-boew/': 'wip/zip/wet-boew.zip'
+                }
+            }
+        },
+        sass: {
+            options: {
+                includePaths: ['node_modules'],
+                implementation: sass
+            },
+            theme: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/wet-boew/stylesheets/base',
+                        src: ['noscript.scss'],
+                        dest: 'dist/theme-' + grunt.config('theme').name + '/css',
+                        ext: '.css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/theme-wet-boew/stylesheets',
+                        src: ['ie8-theme.scss', 'ie8-wet-boew.scss', 'wet-boew.scss', 'theme.scss'],
+                        dest: 'dist/theme-' + grunt.config('theme').name + '/css',
+                        ext: '.css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/wet-boew/stylesheets/polyfills/datalist',
+                        src: ['datalist.scss'],
+                        dest: 'dist/theme-' + grunt.config('theme').name + '/css/polyfills',
+                        ext: '.css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/wet-boew/stylesheets/polyfills/datepicker',
+                        src: ['datepicker.scss'],
+                        dest: 'dist/theme-' + grunt.config('theme').name + '/css/polyfills',
+                        ext: '.css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/wet-boew/stylesheets/polyfills/details',
+                        src: ['details.scss'],
+                        dest: 'dist/theme-' + grunt.config('theme').name + '/css/polyfills',
+                        ext: '.css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/wet-boew/stylesheets/polyfills/meter',
+                        src: ['meter.scss'],
+                        dest: 'dist/theme-' + grunt.config('theme').name + '/css/polyfills',
+                        ext: '.css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/wet-boew/stylesheets/polyfills/progress',
+                        src: ['progress.scss'],
+                        dest: 'dist/theme-' + grunt.config('theme').name + '/css/polyfills',
+                        ext: '.css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/wet-boew/stylesheets/polyfills/slider',
+                        src: ['slider.scss'],
+                        dest: 'dist/theme-' + grunt.config('theme').name + '/css/polyfills',
+                        ext: '.css'
+                    }
+                ]
+            }
+        },       
+        sprite: {
+            prepare: {
+                src: "wip/repo/wet-boew/wet-boew-master/src/plugins/share/sprites/*.png",
+                destCss: "src/plugins/stylesheets/share/_sprites.scss",
+                dest: "src/plugins/stylesheets/share/assets/images/sprites.png"
+            }
+        },
+        'string-replace': {
+            prepare: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: '**/*.scss',
+                        dest: 'src/'
+                    }
+                ],
+                options: {
+                    replacements: [
+                        {
+                            pattern: '@import "sprites/sprites_share";',
+                            replacement: '@import "sprites";'
+                        },
+                        {
+                            pattern: /@import.\"\.\.\/plugins/g,
+                            replacement: '@import "src/plugins/stylesheets'
+                        },
+                        {
+                            pattern: /@import.\"\.\.\/polyfills/g,
+                            replacement: '@import "src/wet-boew/stylesheets/polyfills'
+                        },
+                        {
+                            pattern: /@import.\"\.\.\/variables/g,
+                            replacement: '@import "src/wet-boew/stylesheets/variables'
+                        },
+                        {
+                            pattern: /@import.\"\.\.\/\.\.\/variables/g,
+                            replacement: '@import "src/wet-boew/stylesheets/variables'
+                        },
+                        {
+                            pattern: /@import.\"\.\.\/src\/variables/g,
+                            replacement: '@import "src/wet-boew/stylesheets/variables'
+                        },
+                        {
+                            pattern: /@import.\"\.\.\/src\/base/g,
+                            replacement: '@import "src/wet-boew/stylesheets/base'
+                        },
+                        {
+                            pattern: /@import.\"bootstrap-sass\/assets/g,
+                            replacement: '@import "src/bootstrap'
+                        },
+                        {
+                            pattern: /@extend %#{\$service};/g,
+                            replacement: '@extend %#{$service} !optional;'
+                        }                        
+                    ]
+                }//@extend %#{$service};
+            }
         }
     });
 
-    grunt.registerTask('build', ['clean', 'wget', 'unzip']);
-
+    grunt.registerTask('prepare', ['clean:prepare', 'wget:prepare', 'unzip:prepare', 'copy:prepare', 'string-replace:prepare', 'sprite:prepare']);
+    grunt.registerTask('css', ['sass:theme']);
+    grunt.registerTask('finalize', ['clean:finalize'])
+    grunt.registerTask('build', ['prepare', 'css', 'finalize']);
 }
